5e1b6f1838bde53045635bac39d3f39f7f499de8
chalupa-bazooka <mteplyshev@gmail.com>
2025-01-21 18:20:45 +0400

    Create

---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <div className="container-fluid">
+            <h1 className="mx-auto w-50 text-center">Data Table</h1>
+        </div>
+    )
+}
