92552d2bccb83f6133e9a2a39aae759f09413adf
chalupa-bazooka <mteplyshev@gmail.com>
2025-01-22 20:33:00 +0400

    Revert values back to plain

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
+  apiKey: "AIzaSyAWeFR2-lodFxxANd-bdOeE_avHxYfGiTY",
+  authDomain: "task-4-itransition.firebaseapp.com",
+  projectId: "task-4-itransition",
+  storageBucket: "task-4-itransition.firebasestorage.app",
+  messagingSenderId: "1043058894293",
+  appId: "1:1043058894293:web:9ea04fa2148918568bc95c",
 };
 
 const app = initializeApp(firebaseConfig);
