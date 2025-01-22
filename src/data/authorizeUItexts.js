cbc9b6ecbbc611215d4b7e77bc2dff9fbfd5db81
chalupa-bazooka <mteplyshev@gmail.com>
2025-01-22 20:33:14 +0400

    Move this data out of the Authorize page

---
+++ src/data/authorizeUItexts.js
@@ -0,0 +1,12 @@
+export const conditionalTexts = {
+  login: {
+    h1: "Login",
+    sub: "Don't have an account? Switch to Sign up!",
+    button: "Log in",
+  },
+  signup: {
+    h1: "Sign up",
+    sub: "Already have an account? Switch to Login!",
+    button: "Sign up",
+  },
+};
