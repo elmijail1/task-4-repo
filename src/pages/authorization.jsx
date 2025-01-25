571a1b6558e36955c6551e297c47e18ef6975fa4
chalupa-bazooka <mteplyshev@gmail.com>
2025-01-26 01:43:39 +0400

    Move out handleSignIn

--- src/pages/authorization.jsx
+++ src/pages/authorization.jsx
@@ -1,14 +1,10 @@
 import { useState } from "react"
 import { conditionalTexts } from "../data/authorizeUItexts"
-import { signInWithEmailAndPassword, getAuth, deleteUser } from "firebase/auth"
-import { auth, db } from "../firebase"
-import { doc, collection, getDocs, updateDoc } from "firebase/firestore"
 import { Navigate } from "react-router"
-import validateInput from "../utilities/Authorize/validateInput"
 import registerFocus from "../utilities/Authorize/registerFocus"
 import determineStatusMessage from "../utilities/Authorize/determineStatusMessage"
-import generateRightLengthPassword from "../utilities/Authorize/generateRightLengthPassword"
 import handleSignUp from "../utilities/Authorize/handleSignUp"
+import handleSignIn from "../utilities/Authorize/handleSignIn"
 
 
 export default function Authorization({ user }) {
@@ -47,51 +43,51 @@ export default function Authorization({ user }) {
     // It first checks whether the user's been deleted from the DB
     // If it has, it deletes it from the Auth table too
     // If it's been blocked, it lets you know that you're blocked
-    async function handleSignIn() {
-        const fields = ["email", "password"]
-        for (let field of fields) {
-            validateInput(field, input, setErrorMessages)
-            console.log(errorMessages)
-        }
-        if (!validateInput("email", input, setErrorMessages) || !validateInput("password", input, setErrorMessages)) {
-            setFirstFocus({ name: false, email: true, password: true })
-            setUserStatus("not validated")
-            return
-        }
-        try {
-            let rightLengthPassword = generateRightLengthPassword(input)
-            const userCredential = await signInWithEmailAndPassword(auth, input.email, rightLengthPassword)
-            const user = userCredential.user
-            const usersCollectionRef = collection(db, "users")
-            try {
-                const snapshot = await getDocs(usersCollectionRef)
-                const activeUsersIDs = snapshot.docs.map(doc => ({ ...doc.data(), id: doc.id }))
-                if (!activeUsersIDs.map(entry => entry.id).includes(user.uid)) {
-                    try {
-                        await deleteUser(getAuth().currentUser)
-                    } catch (error) {
-                        console.error(`Error with deletion: ${error} `)
-                    }
-                    setUserStatus("deleted")
-                    setInput({ name: "", email: "", password: "" })
-                    console.log("Your account has been deleted, lol")
-                } else if (activeUsersIDs.filter(entry => entry.id === user.uid)[0].status === "blocked") {
-                    setUserStatus("blocked")
-                    console.log("Your account has been blocked, lol")
-                } else {
-                    await updateDoc(doc(db, "users", user.uid), { lastSeen: new Date() })
-                    setUserStatus("active")
-                }
-            } catch (error) {
-                console.error(`Error with DB: ${error} `)
-            }
-        }
-        catch (error) {
-            console.log(`Error code: ${error.code}`)
-            console.log(`Error message: ${error.message}`)
-            setUserStatus("not found")
-        }
-    }
+    // async function handleSignIn() {
+    //     const fields = ["email", "password"]
+    //     for (let field of fields) {
+    //         validateInput(field, input, setErrorMessages)
+    //         console.log(errorMessages)
+    //     }
+    //     if (!validateInput("email", input, setErrorMessages) || !validateInput("password", input, setErrorMessages)) {
+    //         setFirstFocus({ name: false, email: true, password: true })
+    //         setUserStatus("not validated")
+    //         return
+    //     }
+    //     try {
+    //         let rightLengthPassword = generateRightLengthPassword(input)
+    //         const userCredential = await signInWithEmailAndPassword(auth, input.email, rightLengthPassword)
+    //         const user = userCredential.user
+    //         const usersCollectionRef = collection(db, "users")
+    //         try {
+    //             const snapshot = await getDocs(usersCollectionRef)
+    //             const activeUsersIDs = snapshot.docs.map(doc => ({ ...doc.data(), id: doc.id }))
+    //             if (!activeUsersIDs.map(entry => entry.id).includes(user.uid)) {
+    //                 try {
+    //                     await deleteUser(getAuth().currentUser)
+    //                 } catch (error) {
+    //                     console.error(`Error with deletion: ${error} `)
+    //                 }
+    //                 setUserStatus("deleted")
+    //                 setInput({ name: "", email: "", password: "" })
+    //                 console.log("Your account has been deleted, lol")
+    //             } else if (activeUsersIDs.filter(entry => entry.id === user.uid)[0].status === "blocked") {
+    //                 setUserStatus("blocked")
+    //                 console.log("Your account has been blocked, lol")
+    //             } else {
+    //                 await updateDoc(doc(db, "users", user.uid), { lastSeen: new Date() })
+    //                 setUserStatus("active")
+    //             }
+    //         } catch (error) {
+    //             console.error(`Error with DB: ${error} `)
+    //         }
+    //     }
+    //     catch (error) {
+    //         console.log(`Error code: ${error.code}`)
+    //         console.log(`Error message: ${error.message}`)
+    //         setUserStatus("not found")
+    //     }
+    // }
 
     const [firstFocus, setFirstFocus] = useState({ name: false, email: false, password: false })
     const [errorMessages, setErrorMessages] = useState({ name: "", email: "", password: "" })
@@ -178,12 +174,20 @@ export default function Authorization({ user }) {
                     type="button"
                     className="btn btn-primary w-100"
                     onClick={authType === "login"
-                        ? handleSignIn
-                        : () => handleSignUp(input,
+                        ? () => handleSignIn(
+                            input,
+                            setErrorMessages,
+                            setFirstFocus,
+                            setUserStatus,
+                            setInput
+                        )
+                        : () => handleSignUp(
+                            input,
                             setErrorMessages,
                             setFirstFocus,
                             setUserStatus
-                        )}
+                        )
+                    }
                 >
                     {determineTexts("button")}
                 </button>
---
+++ src/utilities/Authorize/handleSignIn.js
@@ -0,0 +1,68 @@
+import validateInput from "./validateInput";
+import generateRightLengthPassword from "./generateRightLengthPassword";
+import { signInWithEmailAndPassword, deleteUser, getAuth } from "firebase/auth";
+import { auth, db } from "../../firebase";
+import { collection, getDocs, doc, updateDoc } from "firebase/firestore";
+
+export default async function handleSignIn(
+  input,
+  setErrorMessages,
+  setFirstFocus,
+  setUserStatus,
+  setInput
+) {
+  const fields = ["email", "password"];
+  for (let field of fields) {
+    validateInput(field, input, setErrorMessages);
+  }
+  if (
+    !validateInput("email", input, setErrorMessages) ||
+    !validateInput("password", input, setErrorMessages)
+  ) {
+    setFirstFocus({ name: false, email: true, password: true });
+    setUserStatus("not validated");
+    return;
+  }
+  try {
+    let rightLengthPassword = generateRightLengthPassword(input);
+    const userCredential = await signInWithEmailAndPassword(
+      auth,
+      input.email,
+      rightLengthPassword
+    );
+    const user = userCredential.user;
+    const usersCollectionRef = collection(db, "users");
+    try {
+      const snapshot = await getDocs(usersCollectionRef);
+      const activeUsersIDs = snapshot.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      if (!activeUsersIDs.map((entry) => entry.id).includes(user.uid)) {
+        try {
+          await deleteUser(getAuth().currentUser);
+        } catch (error) {
+          console.error(`Error with deletion: ${error} `);
+        }
+        setUserStatus("deleted");
+        setInput({ name: "", email: "", password: "" });
+        console.log("Your account has been deleted, lol");
+      } else if (
+        activeUsersIDs.filter((entry) => entry.id === user.uid)[0].status ===
+        "blocked"
+      ) {
+        setUserStatus("blocked");
+        console.log("Your account has been blocked, lol");
+      } else {
+        await updateDoc(doc(db, "users", user.uid), { lastSeen: new Date() });
+        setUserStatus("active");
+      }
+    } catch (error) {
+      console.error(`Error with DB: ${error} `);
+    }
+  } catch (error) {
+    console.log(`Error code: ${error.code}`);
+    console.log(`Error message: ${error.message}`);
+    setUserStatus("not found");
+  }
+}
