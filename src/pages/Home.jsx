5d1c57db8bb09e9838935d2a819d3a44a3422bd5
chalupa-bazooka <mteplyshev@gmail.com>
2025-01-25 20:07:43 +0400

    Add data sorting by name

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -39,7 +39,19 @@ export default function Home() {
     const usersCollectionRef = collection(db, "users")
     async function populateTable() {
         const snapshot = await getDocs(usersCollectionRef)
-        const users = snapshot.docs.map(doc => ({ ...doc.data(), id: doc.id }))
+        const users = snapshot.docs
+            .map(doc => ({ ...doc.data(), id: doc.id }))
+            .sort((a, b) => {
+                let valueA = a.name
+                let valueB = b.name
+                if (valueA < valueB) {
+                    return -1
+                } else if (valueA > valueB || valueA === valueB) {
+                    return 1
+                } else {
+                    return 0
+                }
+            })
         setTableData(users)
     }
 
