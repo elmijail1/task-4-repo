9a243bc7d011d9c1571dcb85fba812ff4071125c
chalupa-bazooka <mteplyshev@gmail.com>
2025-01-25 20:30:43 +0400

    Add handling of a 1-char long minute value

--- src/utilities/Home/typeDate.js
+++ src/utilities/Home/typeDate.js
@@ -1,6 +1,11 @@
 export default function typeDate(timestamp) {
   const date = timestamp.toDate();
+  let minutes = date.getMinutes();
+  if (minutes.length === 1) {
+    minutes = `0${minutes}`;
+  }
+
   return `${date.getDate()}/${
     date.getMonth() + 1
-  }/${date.getFullYear()} at ${date.getHours()}:${date.getMinutes()}`;
+  }/${date.getFullYear()} at ${date.getHours()}:${minutes}`;
 }
