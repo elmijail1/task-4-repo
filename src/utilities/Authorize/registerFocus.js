4fbd0e07c3b524d6ce0fed4ab4abcaa82ee070d2
chalupa-bazooka <mteplyshev@gmail.com>
2025-01-26 01:33:01 +0400

    Move out

--- src/utilities/Authorize/registerFocus.js
+++ src/utilities/Authorize/registerFocus.js
@@ -1,5 +1,16 @@
-export default function registerFocus(event, firstFocus, setFirstFocus) {
+export default function registerFocus(
+  event,
+  firstFocus,
+  setFirstFocus,
+  userStatus,
+  setUserStatus
+) {
   if (!firstFocus[event.target.name]) {
     setFirstFocus((prevFocus) => ({ ...prevFocus, [event.target.name]: true }));
+  }
+
+  if (userStatus === "not validated") {
+    setUserStatus("");
+    console.log(userStatus);
   }
 }
